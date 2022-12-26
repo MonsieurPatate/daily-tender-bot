57df6ee753bab758fdce64416c42aa7d0753f999
iCatOK <kamilyen5@gmail.com>
2022-12-26 15:37:35 +0500

    Добавлено голосование

--- app/database/config.py
+++ app/database/config.py
@@ -1,3 +1,5 @@
 from peewee import *
 
 db = SqliteDatabase('main.db')
+
+bot_token = '5806300117:AAHRdpP_8SF-Mw1UM9xBi3omSwBSneRt9fs'
--- app/main.py
+++ app/main.py
@@ -1,10 +1,13 @@
 import telebot
 
+from app.database.config import db
+from app.database.models import Member, ChatConfig, TenderParticipant
 from bot import bot
 
 
 if __name__ == '__main__':
-
+    # db.drop_tables([Member, ChatConfig, TenderParticipant])
+    db.create_tables([Member, ChatConfig, TenderParticipant])
     bot.set_my_commands([
         telebot.types.BotCommand("/start", "Запуск бота"),
         telebot.types.BotCommand("/add", "Добавление пользователей"),
