bae80a167a4fc3f31b45e882408407f0973a1f77
Pomme de terre <MonsieurPatate@gmail.com>
2023-01-23 23:48:49 +0500

    fix work in docker container

--- app/main.py
+++ app/main.py
@@ -2,10 +2,6 @@ import os
 import sys
 import telebot
 
-from app.config import db
-from app.orm_models.models import Member, ChatConfig, TenderParticipant
-from bot import bot
-
 current_dir = os.path.dirname(os.path.realpath(__file__))
 main_folder_path = os.path.dirname(current_dir)
 parent_dir = os.path.dirname(main_folder_path)
@@ -13,6 +9,10 @@ sys.path.append(os.path.dirname(parent_dir))
 sys.path.append(current_dir)
 sys.path.append(parent_dir)
 sys.path.append(main_folder_path)
+
+from app.config import db
+from app.orm_models.models import Member, ChatConfig, TenderParticipant
+from bot import bot
 
 
 if __name__ == '__main__':
